cc4fcda22c710dbc4ddd62975263e63c534df035
Peter Pham <s4904076@student.uq.edu.au>
2025-05-20 11:07:35 +1000

    made overlay and started slightly on the include files, not working

---
+++ door_node/include/m24sr64.h
@@ -0,0 +1,8 @@
+
+#ifndef M24SR64_H
+#define M24SR64_H
+
+
+void NFCRead(void);
+
+#endif // M24SR64_H
---
+++ door_node/lib/m24sr64.c
@@ -0,0 +1,9 @@
+
+#include "m24sr64.h.h"
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/autoconf.h>
+#include <zephyr/drivers/i2c.h>
+
+
